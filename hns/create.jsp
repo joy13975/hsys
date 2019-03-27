db85b085d3a23ba763faf5cbedbb20626ceca6db
Joy Yeh <joyyeh.tw@gmail.com>
2019-03-27 14:46:19 +0900

    Aggregated initialization code and updated looks to a satisfactory state.

--- hns/create.jsp
+++ hns/create.jsp
@@ -103,7 +103,7 @@
                         <label class="pt-2 float-right">所属</label>
                     </div>
                     <div class="col-10">
-                        <input type="text" class="form-control" name="employee" placeholder="例：株式会社HNS" value="株式会社HNS" required>
+                        <input type="text" class="form-control" name="employer" placeholder="例：株式会社HNS" value="株式会社HNS" required>
                     </div>
                 </div>
                 <div class="row">
@@ -124,6 +124,7 @@
                         </div>
                     </div>
                 </div>
+                <div class="row m-3"></div>
                 <div class="row">
                     <div class="col-2">
                         <label class="pt-2 float-right">住所郵便番号</label>
@@ -136,17 +137,19 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-2">
+                    <div class="col-md-2">
                         <label class="pt-2 float-right">町番地号</label>
                     </div>
-                    <div class="col-2">
+                    <div class="col-md-10">
                         <input type="text" class="form-control" name="address2" placeholder="例：1-3-6" required>
                         <span></span>
                     </div>
-                    <div class="col-3">
-                        <label class="pt-2 float-right">建物名前と部屋番号</label>
+                </div>
+                <div class="row">
+                    <div class="col-md-2">
+                        <label class="pt-2 float-right">建物・室号</label>
                     </div>
-                    <div class="col-5">
+                    <div class="col-md-10">
                         <input type="text" class="form-control" name="address3" placeholder="例：最高マンション111号" required>
                     </div>
                 </div>
@@ -171,6 +174,7 @@
                         </div>
                     </div>
                 </div>
+                <div class="row m-3"></div>
                 <div class="row">
                     <div class="col-2">
                         <label class="pt-2 float-right">最終学歴</label>
@@ -192,7 +196,7 @@
             <div id="qualifications">
                 <h2 class="pt-2">資格．研修</h2>
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col">
                         <div class="card">
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item">
@@ -208,44 +212,33 @@
                                         </div>
                                     </div>
                                 </li>
-                                <div class="table-splitter"></div>
-                                <!-- <li class="list-group-item">
-                                    <div class="row" id="qual_row">
-                                        <div class="col-7">
-                                            <input type="text" class="form-control" placeholder="例：普通自動車運転免許" required>
-                                        </div>
-                                        <div class="col-3">
-                                            <input type="date" class="form-control">
-                                        </div>
-                                        <div class="col-2 text-center">
-                                            <button type="button" class="btn btn-danger fa fa-times-circle table-button" onclick="delete_row(this);"></button>
-                                        </div>
-                                    </div>
-                                </li> -->
+                                <table-splitter>
                             </ul>
                         </div>
                     </div>
                 </div>
                 <div class="row pt-2">
-                    <div class="col-12 text-center">
-                        <button type="button" class="btn btn-info fa fa-plus table-button" onclick="add_row(this, 'qualification');"></button>
+                    <div class="col text-center">
+                        <button type="button" class="btn btn-info fa fa-plus table-button" onclick="addRow(this, 'qualification');"></button>
                     </div>
                 </div>
             </div>
             <hr>
             <div id="experiences">
                 <h2 class="pt-2">経歴</h2>
-                <small class="form-text text-muted">*職務：PM（プロジェクトマネージャー）　PL（プロジェクトリーダー）　SE（システム．エンジニア） SE/PG（SE兼PG） PG（プログラマー） OP（オプレータ）</small>
                 <div class="row">
                     <div class="card">
                         <ul class="list-group list-group-flush">
-                            <div class="table-splitter"></div>
+                            <li class="list-group-item">
+                                <small class="form-text text-muted">*職務：PM（プロジェクトマネージャー）　PL（プロジェクトリーダー）　SE（システム．エンジニア） SE/PG（SE兼PG） PG（プログラマー） OP（オプレータ）</small>
+                            </li>
+                            <table-splitter>
                         </ul>
                     </div>
                 </div>
                 <div class="row pt-2">
-                    <div class="col-12 text-center">
-                        <button type="button" class="btn btn-info fa fa-plus table-button" onclick="add_row(this, 'experience');"></button>
+                    <div class="col text-center">
+                        <button type="button" class="btn btn-info fa fa-plus table-button" onclick="addRow(this, 'experience');"></button>
                     </div>
                 </div>
             </div>
--- hns/js/create.js
+++ hns/js/create.js
@@ -4,9 +4,7 @@ let today = new Date();
 let today_date_str = today.toISOString().slice(0, 10);
 
 $(function() {
-    // Initialize datepickers
-    let dateEles = $('input[is-datepicker=yes]');
-    initDatepicker(dateEles);
+    initSpecialInputs($('html'));
 
     // Auto update age.
     let ageText = $("#age_text");
@@ -15,22 +13,21 @@ $(function() {
         yd = new Date(today - new Date(bdPicker.val())).getFullYear() - 1970;
         ageText.html(yd + "歳");
     });
-
-    // Set min heights for all textarea.
-    tas = $('textarea');
-    tas.css('min-height', tas.height() + "px");
-
-    // Initialize tokenfields.
-    let tfs = $('input[tokenfield=yes]');
-    tfs.tokenfield();
-    tfs.html("");
 })
 
 function onNumberFocusOut(ele, min, max, step) {
     console.log(ele, "min=" + min, "max=" + max, "step=" + step);
 }
 
-function initDatepicker(dp) {
+function initSpecialInputs(root) {
+    initDatepicker(root);
+    initTokenfields(root);
+    initTextareas(root);
+}
+
+function initDatepicker(root) {
+    // Set format and default value for all datepickers in root.
+    let dp = $(root).find('input[is-datepicker=yes]')
     dp.datepicker({
         format: 'yyyy/mm/dd',
         todayHighlight: true
@@ -39,13 +36,27 @@ function initDatepicker(dp) {
     dp.datepicker("update", today);
 }
 
+function initTokenfields(root) {
+    // Call initialization method for all tokenfields in root element.
+    let tf = $(root).find('input[is-tokenfield=yes]');
+    tf.tokenfield({
+        delimiter: [',', ' ']
+    });
+}
+
+function initTextareas(root) {
+    // Set min heights for all textarea in root element.
+    ta = $(root).find('textarea');
+    ta.css('min-height', ta.height() + "px");
+}
+
 function deleteRow(ele) {
     $(ele).parents('li').remove();
 }
 
 function addRow(ele, template) {
     // Insert after header if there are no existing rows, else insert after last row.
-    let splitter = $(ele).parent().parent().prev().find('.table-splitter');
+    let splitter = $(ele).parent().parent().prev().find('table-splitter');
     let rows = splitter.nextAll('li');
 
     var insertAfter = splitter;
@@ -73,18 +84,17 @@ function addQualRow(insertAfter) {
     function ri(name) {
         return name + '-' + qualRowID;
     }
-    dpName = ri('qual_date');
 
-    row = $(`<li class="list-group-item">
+    let row = $(`<li class="list-group-item">
         <div class="row">
             <div class="col-1">
-            	<h4><span class="badge badge-secondary span-center-text" style="display: flex">#` + qualRowID + `</span></h4>
+            	<h4><span class="badge badge-secondary span-center-text row-badge">#` + qualRowID + `</span></h4>
             </div>
             <div class="col-6">
                 <input type="text" class="form-control" name="` + ri('qual_name') + `" placeholder="例：普通自動車運転免許" required>
             </div>
             <div class="col-3">
-                <input type="text" is-datepicker="yes" name="` + dpName + `" class="form-control" required>
+                <input type="text" is-datepicker="yes" name="` + ri('qual_date') + `" class="form-control" required>
             </div>
             <div class="col-2 text-center">
                 <button type="button" class="btn btn-danger fa fa-times-circle table-button" onclick="deleteRow(this);"></button>
@@ -93,9 +103,7 @@ function addQualRow(insertAfter) {
     </li>`);
 
     insertAfter.after(row);
-
-    dp = $("input[name=" + dpName + "]");
-    initDatepicker(dp);
+    initSpecialInputs(row);
 }
 
 let expRowID = 0;
@@ -106,8 +114,6 @@ function addExpRow(insertAfter) {
     function ri(name) {
         return name + '-' + expRowID;
     }
-    startDpName = ri('case_start');
-    endDpName = ri('case_end')
 
     cbAnalysisID = ri('cb_analysis');
     cbReqDefID = ri('cb_req_def');
@@ -120,10 +126,10 @@ function addExpRow(insertAfter) {
     cbMaintenanceAndApplicationID = ri('cb_maintenance_and_application');
     cbHelpdeskID = ri('cb_helpdesk');
 
-    row = $(`
+    let row = $(`
 		<li class="list-group-item">
 			<div class="row">
-				<h4><span class="badge badge-secondary span-center-text" style="display: flex">#` +
+				<h4><span class="badge badge-secondary span-center-text row-badge ml-2">#` +
         expRowID + `</span></h4>
 			</div>
 		    <div class="row">
@@ -133,20 +139,20 @@ function addExpRow(insertAfter) {
 		                    <label class="pt-2 float-right">開始</label>
 		                </div>
 		                <div class="col-4">
-		                    <input type="text" is-datepicker="yes" name="` + startDpName + `" class="form-control">
+		                    <input type="text" is-datepicker="yes" name="` + ri('case_start') + `" class="form-control">
 		                </div>
-		                <div class="col-1">
+		                <div class="col-2">
 		                    <label class="pt-2 float-right">終了</label>
 		                </div>
 		                <div class="col-4">
-		                    <input type="text" is-datepicker="yes" name="` + endDpName + `" class="form-control">
+		                    <input type="text" is-datepicker="yes" name="` + ri('case_end') + `" class="form-control">
 		                </div>
 		            </div>
 		            <div class="row" name="` + ri('case_name') + `">
 		                <div class="col-2">
 		                    <label class="pt-2 float-right">案件名</label>
 		                </div>
-		                <div class="col-9">
+		                <div class="col-10">
 		                    <input type="text" class="form-control" placeholder="例：〇〇ソフトウェアエンジニア" required>
 		                </div>
 		            </div>
@@ -154,7 +160,7 @@ function addExpRow(insertAfter) {
 		                <div class="col-2">
 		                    <label class="pt-2 float-right">内容</label>
 		                </div>
-		                <div class="col-9">
+		                <div class="col-10">
 		                    <textarea class="form-control" rows="3" min required></textarea>
 		                </div>
 		            </div>
@@ -162,65 +168,65 @@ function addExpRow(insertAfter) {
 		                <div class="col-2">
 		                    <label class="pt-2 float-right">業務</label>
 		                </div>
-		                <div class="col-9">
+		                <div class="col-10">
 		                    <div class="row group-border m-0 pb-2">
-		                        <div class="col-12">
+		                        <div class="col">
 		                            <div class="row ml-0 mr-0">
-		                                <div class="form-check">
+		                                <div class="col-3 form-check">
 		                                    <input class="form-check-input" type="checkbox" value="" id="` + cbAnalysisID + `" name="` + cbAnalysisID + `">
 		                                    <label class="form-check-label" for="` + cbAnalysisID + `">
 		                                        調査分析
 		                                    </label>
 		                                </div>
-		                                <div class="form-check">
+		                                <div class="col-3 form-check">
 		                                    <input class="form-check-input" type="checkbox" value="" id="` + cbReqDefID + `" name="` + cbReqDefID + `">
 		                                    <label class="form-check-label" for="` + cbReqDefID + `">
 		                                        要件定義
 		                                    </label>
 		                                </div>
-		                                <div class="form-check">
+		                                <div class="col-3 form-check">
 		                                    <input class="form-check-input" type="checkbox" value="" id="` + cbBasicDesignID + `" name="` + cbBasicDesignID + `">
 		                                    <label class="form-check-label" for="` + cbBasicDesignID + `">
 		                                        基本設計
 		                                    </label>
 		                                </div>
-		                                <div class="form-check">
+		                                <div class="col-3 form-check">
 		                                    <input class="form-check-input" type="checkbox" value="" id="` + cbDetailedDesignID + `" name="` + cbDetailedDesignID + `">
 		                                    <label class="form-check-label" for="` + cbDetailedDesignID + `">
 		                                        詳細設計
 		                                    </label>
 		                                </div>
-		                                <div class="form-check">
+		                                <div class="col-3 form-check">
 		                                    <input class="form-check-input" type="checkbox" value="" id="` + cbProgrammingID + `" name="` + cbProgrammingID + `">
 		                                    <label class="form-check-label" for="` + cbProgrammingID + `">
 		                                        プログラミング
 		                                    </label>
 		                                </div>
-		                                <div class="form-check">
+		                                <div class="col-3 form-check">
 		                                    <input class="form-check-input" type="checkbox" value="" id="` + cbUnitTestID + `" name="` + cbUnitTestID + `">
 		                                    <label class="form-check-label" for="` + cbUnitTestID + `">
 		                                        単体テスト
 		                                    </label>
 		                                </div>
-		                                <div class="form-check">
+		                                <div class="col-3 form-check">
 		                                    <input class="form-check-input" type="checkbox" value="" id="` + cbIntegratedTestID + `" name="` + cbIntegratedTestID + `">
 		                                    <label class="form-check-label" for="` + cbIntegratedTestID + `">
 		                                        結合テスト
 		                                    </label>
 		                                </div>
-		                                <div class="form-check">
+		                                <div class="col-3 form-check">
 		                                    <input class="form-check-input" type="checkbox" value="" id="` + cbOverallTestID + `" name="` + cbOverallTestID + `">
 		                                    <label class="form-check-label" for="` + cbOverallTestID + `">
 		                                        総合テスト
 		                                    </label>
 		                                </div>
-		                                <div class="form-check">
+		                                <div class="col-3 form-check">
 		                                    <input class="form-check-input" type="checkbox" value="" id="` + cbMaintenanceAndApplicationID + `" name="` + cbMaintenanceAndApplicationID + `">
 		                                    <label class="form-check-label" for="` + cbMaintenanceAndApplicationID + `">
 		                                        保守．運用
 		                                    </label>
 		                                </div>
-		                                <div class="form-check">
+		                                <div class="col-3 form-check">
 		                                    <input class="form-check-input" type="checkbox" value="" id="` + cbHelpdeskID + `" name="` + cbHelpdeskID + `">
 		                                    <label class="form-check-label" for="` + cbHelpdeskID + `">
 		                                        ヘルプデスク
@@ -228,10 +234,10 @@ function addExpRow(insertAfter) {
 		                                </div>
 		                            </div>
 		                            <div class="row mt-2">
-		                                <div class="col-2">
+		                                <div class="col-3">
 		                                    <label class="pt-2">その他</label>
 		                                </div>
-		                                <div class="col-10">
+		                                <div class="col-9">
 		                                    <input type="text" class="form-control" name="` + ri('task_other') + `">
 		                                </div>
 		                            </div>
@@ -241,22 +247,25 @@ function addExpRow(insertAfter) {
 		            </div>
 		            <div class="row" name="scale">
 		                <div class="col-2">
-		                    <label class="pt-2 float-right">全体規模</label>
+		                    <label class="pt-2 float-right">規模</label>
 		                </div>
-		                <div class="col-4">
+		                <div class="col-2">
+		                    <label class="pt-2 float-right">全体</label>
+		                </div>
+		                <div class="col-3">
 		                    <div class="input-group">
-		                        <input name="total_scale" type="number" onfocusout="onNumberFocusOut(this, 1, "none", 1)" step="1" min="1" value="1" class="form-control">
+		                        <input name="` + ri('total_scale') + `" type="number" onfocusout="onNumberFocusOut(this, 1, "none", 1)" step="1" min="1" value="1" class="form-control">
 		                        <span class="input-group-append">
 		                            <span class="input-group-text" id="total_scale_text">人</span>
 		                        </span>
 		                    </div>
 		                </div>
-		                <div class="col-1">
+		                <div class="col-2">
 		                    <label class="pt-2 float-right">チーム</label>
 		                </div>
-		                <div class="col-4">
+		                <div class="col-3">
 		                    <div class="input-group">
-		                        <input name="team_scale" type="number" onfocusout="onNumberFocusOut(this, 1, "none", 1)" step="1" min="1" value="1" class="form-control">
+		                        <input name="` + ri('team_scale') + `" type="number" onfocusout="onNumberFocusOut(this, 1, "none", 1)" step="1" min="1" value="1" class="form-control">
 		                        <span class="input-group-append">
 		                            <span class="input-group-text" id="team_scale_text">人</span>
 		                        </span>
@@ -267,9 +276,9 @@ function addExpRow(insertAfter) {
 		                <div class="col-2">
 		                    <label class="pt-2 float-right">機種OS</label>
 		                </div>
-		                <div class="col-9">
+		                <div class="col-10">
 		                    <div class="input-group">
-		                        <input name="os" type="text" tokenfield="yes" class="form-control">
+		                        <input name="` + ri('os') + `" type="text" is-tokenfield="yes" class="form-control">
 		                    </div>
 		                </div>
 		            </div>
@@ -277,19 +286,19 @@ function addExpRow(insertAfter) {
 		                <div class="col-2">
 		                    <label class="pt-2 float-right">言語やツール</label>
 		                </div>
-		                <div class="col-9">
+		                <div class="col-10">
 		                    <div class="input-group">
-		                        <input name="proglang_tools" type="text" tokenfield="yes" class="form-control">
+		                        <input name="` + ri('proglang_tools') + `" type="text" is-tokenfield="yes" class="form-control">
 		                    </div>
 		                </div>
 		            </div>
 		            <div class="row" name="db_types">
 		                <div class="col-2">
-		                    <label class="float-right" style="text-align: right">DBや<br>DCミドルウェア</label>
+		                    <label class="float-right" style="text-align: right">DBMSミドルウェア</label>
 		                </div>
-		                <div class="col-9">
+		                <div class="col-10">
 		                    <div class="input-group">
-		                        <input name="db" type="text" tokenfield="yes" class="form-control">
+		                        <input name="` + ri('db') + `" type="text" is-tokenfield="yes" class="form-control">
 		                    </div>
 		                </div>
 		            </div>
@@ -302,9 +311,5 @@ function addExpRow(insertAfter) {
 	`);
 
     insertAfter.after(row);
-
-    start_dp = $("input[name=" + startDpName + "]");
-    end_dp = $("input[name=" + endDpName + "]");
-    initDatepicker(start_dp);
-    initDatepicker(end_dp);
+    initSpecialInputs(row);
 }
