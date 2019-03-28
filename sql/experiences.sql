993b5cabfba53f0e0be3834909410639a501cb0c
Joy Yeh <joyyeh.tw@gmail.com>
2019-03-28 17:02:43 +0900

    Fleshed out sql schemes. Skip nosql for now.

--- sql/experiences.sql
+++ sql/experiences.sql
@@ -1,4 +1,24 @@
 CREATE TABLE experiences(
 	id INT AUTO_INCREMENT,
-	PRIMARY KEY (id)
+	personnel_id INT NOT NULL,
+	case_start DATE NOT NULL,
+	case_end DATE NOT NULL,
+	case_name VARCHAR(128) NOT NULL,
+	case_desc TEXT NOT NULL,
+	cb_req_def BOOLEAN NOT NULL,
+	cb_basic_design BOOLEAN NOT NULL,
+	cb_detailed_design BOOLEAN NOT NULL,
+	cb_programming BOOLEAN NOT NULL,
+	cb_unit_test BOOLEAN NOT NULL,
+	cb_helpdesk BOOLEAN NOT NULL,
+	task_other VARCHAR(256) NOT NULL,
+	total_scale INT NOT NULL,
+	team_scale INT NOT NULL,
+	os VARCHAR(512) NOT NULL,
+	proglang_tools VARCHAR(512) NOT NULL,
+	dbms VARCHAR(512) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (personnel_id) 
+		REFERENCES personnel(id)
+		ON DELETE CASCADE
 );
--- sql/qualification.sql
+++ sql/qualification.sql
@@ -1,4 +1,10 @@
 CREATE TABLE qualification(
 	id INT AUTO_INCREMENT,
-	PRIMARY KEY (id)
+	personnel_id INT NOT NULL,
+	qual_name VARCHAR(128) NOT NULL,
+	qual_date DATE NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (personnel_id) 
+		REFERENCES personnel(id)
+		ON DELETE CASCADE
 );
